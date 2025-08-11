6b27aa554b7da44be0142bfcade6e08511920cd8
Satyendra Deversetti <satyendra332@Satyendras-MacBook-Pro.local>
2025-08-11 08:52:25 -0400

    Improved readability"

--- find-no-of-emails-in-a-file/main.py
+++ find-no-of-emails-in-a-file/main.py
@@ -1,19 +1,25 @@
 fna=input("please enter the name of file:")
 fop=open(fna)
+
 counts=dict()
-wo=[]
+wo=list()
+
 for line in fop:
     if 'From' not in line:
         continue
     words=line.split()
     if len(words)>2:
         wo.append(words[1])
+
 for w in wo:
     counts[w]=counts.get(w,0)+1
+
 highcount=0
 highword=0
+
 for word,count in counts.items():
     if word==0 or highcount<count:
         highcount=count
         highword=word
+
 print(highword,highcount)
